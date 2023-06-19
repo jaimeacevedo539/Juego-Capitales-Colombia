5e815e93f666d023c0282d40f919885b5a409eb8
Alejandro Gonzalez <alejandro@kenssey.com>
2023-06-19 17:38:38 -0500

    Actualizacion Cambio Escenas

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
+
     @FXML
     private Label labelDepartamento;
     @FXML
@@ -31,25 +38,24 @@ public class HelloController {
 
         labelDescripcion.getChildren().add(Desc);
 
-        //campoTexto.setText("");
-        //numero = (int)(Math.random()*30+1);
-
-        //Departamento = matriz[numero][0];
         Departamento = pregunta1.getDepartamento();
         Capital = pregunta1.getCapital();
         //Descripcion = matriz2[numero][2];
         //aleatorio.setText(Capital);
-        RandomMap = pregunta1.getCodigo()+".png";
+        RandomMap = pregunta1.getCodigo() + ".png";
         Image mapaMostrar = new Image(getClass().getResourceAsStream(RandomMap));
         mapa.setImage(mapaMostrar);
         //labelDescripcion.setText(pregunta1.getDescripcion());
 
         //departamento.setText(Departamento);
-        System.out.println(pregunta1.getCodigo() +" - " + pregunta1.getDepartamento() +" - " + pregunta1.getCapital());
+        System.out.println(pregunta1.getCodigo() + " - " + pregunta1.getDepartamento() + " - " + pregunta1.getCapital());
         System.out.println(pregunta1.generarOpciones());
         System.out.println(Desc);
 
 
     }
+
+
+
 
 }
---
+++ src/main/java/com/example/demo/SceneSwitch.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class SceneSwitch {
+    public SceneSwitch(AnchorPane currentAnchorPane, String fxml) throws IOException{
+        AnchorPane nextAnchorPane = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource(fxml)));
+        currentAnchorPane.getChildren().removeAll();
+        currentAnchorPane.getChildren().setAll(nextAnchorPane);
+
+    }
+}
