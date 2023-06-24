242f7982edb961245feca6fb84d533ed9a22f69f
Johan Perez <ingjohanperez@hotmail.com>
2023-06-24 00:25:13 -0500

    implementacion de clases Jugador e Historial

--- src/main/java/ppb/grupo13/juegoCapitalesColombia/HelloApplication.java
+++ src/main/java/ppb/grupo13/juegoCapitalesColombia/HelloApplication.java
@@ -12,12 +12,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene2.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene2.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Capitales de Colombia 1");
         stage.setResizable(false);
         stage.setScene(scene);
-        stage.show();*/
+        stage.show();
 
         Historial h = new Historial();
         h.getPuntajesOrdenados();
