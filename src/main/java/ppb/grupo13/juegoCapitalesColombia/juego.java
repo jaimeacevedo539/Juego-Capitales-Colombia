c5ca5881d3a3e853584f108327e531522151ba47
HECTOR ANIBAL LOPEZ LOPEZ <134507441+hall361@users.noreply.github.com>
2023-06-25 09:42:50 -0500

    Update juego.java
    JavaDoc Juego

--- src/main/java/ppb/grupo13/juegoCapitalesColombia/juego.java
+++ src/main/java/ppb/grupo13/juegoCapitalesColombia/juego.java
@@ -1,20 +1,28 @@
 package ppb.grupo13.juegoCapitalesColombia;
 
+/**
+ * Clase para representar el juego "Capitales de Colombia".
+ */
 public class juego {
     //constructor
     public juego(){}
     int cantidadPreguntas = 10;
     int puntaje = 0;
 
+    /**
+     * Método para generar una nueva pregunta.
+     * @return una nueva pregunta.
+     */
     public pregunta generarPregunta(){
         pregunta nuevaPregunta = new pregunta();
         return nuevaPregunta;
     }
 
+    /**
+     * Método para establecer el puntaje del juego.
+     */
     public void setScore(){
 
     }
 
 }
-
-
