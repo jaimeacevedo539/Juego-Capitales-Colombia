bc7a58ff784173844fb94f1fa9e55cd36c84679a
HECTOR ANIBAL LOPEZ LOPEZ <134507441+hall361@users.noreply.github.com>
2023-06-25 10:16:24 -0500

    Update SceneSwitch.java
    JavaDoc SceneSwitch

--- src/main/java/ppb/grupo13/juegoCapitalesColombia/SceneSwitch.java
+++ src/main/java/ppb/grupo13/juegoCapitalesColombia/SceneSwitch.java
@@ -6,7 +6,16 @@ import javafx.scene.layout.AnchorPane;
 import java.io.IOException;
 import java.util.Objects;
 
+/**
+ * Clase para cambiar entre escenas en el juego "Capitales de Colombia".
+ */
 public class SceneSwitch {
+    /**
+     * Constructor para cambiar entre escenas.
+     * @param currentAnchorPane el AnchorPane actual.
+     * @param fxml el archivo FXML de la siguiente escena.
+     * @throws IOException si ocurre un error al cargar el archivo FXML.
+     */
     public SceneSwitch(AnchorPane currentAnchorPane, String fxml) throws IOException{
         AnchorPane nextAnchorPane = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource(fxml)));
         currentAnchorPane.getChildren().removeAll();
