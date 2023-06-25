807d0aadd0e4057f2dcf71f502d68c2a23d756f7
HECTOR ANIBAL LOPEZ LOPEZ <134507441+hall361@users.noreply.github.com>
2023-06-25 10:07:25 -0500

    Update Scene2Controller.java
    JavaDoc

--- src/main/java/ppb/grupo13/juegoCapitalesColombia/Scene2Controller.java
+++ src/main/java/ppb/grupo13/juegoCapitalesColombia/Scene2Controller.java
@@ -7,6 +7,9 @@ import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
+/**
+ * Controlador para la segunda escena del juego "Capitales de Colombia".
+ */
 public class Scene2Controller {
 
     @FXML
@@ -15,6 +18,11 @@ public class Scene2Controller {
     @FXML
     private Button newGame;
 
+    /**
+     * Método para manejar el evento del botón "Inicio".
+     * @param event el evento del botón.
+     * @throws IOException si ocurre un error al cambiar de escena.
+     */
     @FXML
     void inicio(ActionEvent event) throws IOException {
         new SceneSwitch(Scene2AnchorPane, "Scene3.fxml");
