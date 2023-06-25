27097b90dc64b9689a8c1d48c7dbf3a5f4eec0df
Johan Perez <ingjohanperez@hotmail.com>
2023-06-25 01:09:32 -0500

    implementacion de clases Pregunta y Departamento finalizadas

--- src/main/java/ppb/grupo13/juegoCapitalesColombia/HelloApplication.java
+++ src/main/java/ppb/grupo13/juegoCapitalesColombia/HelloApplication.java
@@ -6,24 +6,39 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import ppb.grupo13.juegoCapitalesColombia.model.Historial;
 import ppb.grupo13.juegoCapitalesColombia.model.Jugador;
+import ppb.grupo13.juegoCapitalesColombia.model.Pregunta;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene2.fxml"));
+        /* FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene2.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Capitales de Colombia 1");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+        */
 
+        /*
         Historial h = new Historial();
         h.getPuntajesOrdenados();
 
         Jugador j = new Jugador("Fer",30);
         h.agregarJugador(j);
+        */
+
+        Pregunta p = new Pregunta("Cundinamarca");
+        System.out.println(p.getPregunta());
+        System.out.println(p.getRespuestaCorrecta());
+        System.out.println(p.getPuntaje());
+        System.out.println("opciones: ");
+
+        for (Object g:p.getOpciones()) {
+            System.out.println(g);
+        }
+
     }
 
     public static void main(String[] args) {
