79b95a2eb0aa23db0521a35d77e00b49cb3663a8
HECTOR ANIBAL LOPEZ LOPEZ <134507441+hall361@users.noreply.github.com>
2023-06-25 20:16:21 -0500

    Update Pregunta.java
    Pregunta

--- src/main/java/ppb/grupo13/juegoCapitalesColombia/model/Pregunta.java
+++ src/main/java/ppb/grupo13/juegoCapitalesColombia/model/Pregunta.java
@@ -6,6 +6,9 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+/**
+ * Clase para representar una pregunta en el juego "Capitales de Colombia".
+ */
 public class Pregunta {
 
     private final String JSON_DPTS = "src/main/resources/ppb/grupo13/juegoCapitalesColombia/JsonFiles/Departamentos.json";
@@ -14,6 +17,10 @@ public class Pregunta {
     private ArrayList<String> opciones;
     private int puntaje;
 
+    /**
+     * Constructor para la clase Pregunta que recibe un departamento.
+     * @param departamento el departamento para el cual se genera la pregunta.
+     */
     public Pregunta(String departamento) {
         setPregunta("la capital del departamento "+departamento+" es:");
         setRespuestaCorrecta();
@@ -21,18 +28,34 @@ public class Pregunta {
         setPuntaje(10);
     }
 
+    /**
+     * Método para obtener la pregunta actual.
+     * @return la pregunta actual.
+     */
     public String getPregunta() {
         return pregunta;
     }
 
+    /**
+     * Método para establecer la pregunta actual.
+     * @param pregunta la pregunta actual.
+     */
     private void setPregunta(String pregunta) {
         this.pregunta = pregunta;
     }
 
+    /**
+     * Método para obtener la respuesta correcta a la pregunta actual.
+     * @return la respuesta correcta a la pregunta actual.
+     */
     public String getRespuestaCorrecta() {
         return respuestaCorrecta;
     }
 
+    /**
+     * Método para leer las opciones de respuesta a la pregunta actual desde un archivo JSON.
+     * @param departamento el departamento para el cual se generan las opciones de respuesta.
+     */
     private void leerOpciones(String departamento){
 
         try {
@@ -78,22 +101,40 @@ public class Pregunta {
         }
     }
 
+    /**
+     * Método para establecer la respuesta correcta a la pregunta actual.
+     */
     private void setRespuestaCorrecta() {
         this.respuestaCorrecta = respuestaCorrecta;
     }
 
+    /**
+     * Método para obtener las opciones de respuesta a la pregunta actual.
+     * @return un ArrayList con las opciones de respuesta a la pregunta actual.
+     */
     public ArrayList getOpciones() {
         return opciones;
     }
-
+    /**
+     * Método para establecer las opciones de respuesta a la pregunta actual.
+     * @param respuestas un ArrayList con las opciones de respuesta a la pregunta actual.
+     */
     private void setOpciones(ArrayList respuestas) {
         this.opciones = respuestas;
     }
 
+    /**
+     * Método para obtener el puntaje que otorga la pregunta actual.
+     * @return el puntaje que otorga la pregunta actual.
+     */
     public int getPuntaje() {
         return puntaje;
     }
 
+    /**
+     * Método para establecer el puntaje que otorga la pregunta actual.
+     * @param puntaje el puntaje que otorga la pregunta actual.
+     */
     private void setPuntaje(int puntaje) {
         this.puntaje = puntaje;
     }
